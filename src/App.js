172449fc847aab8a61612ebaf171797ed8d764e3
unknown <slimghoul2@gmail.com>
2021-04-03 09:52:03 -0700

    seconde react jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import myImage from "./isrc.jpg"
+
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">Slim El Ghoul</h1>
+    <br />
+    <img src={myImage} width={1000} height={400} />
+    <br />
+    <img src="/ipublic.jpg" width={1000} height={400}/>
+  </div>
+  <video width={320} height={240} controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</div>
+
     </div>
   );
 }
